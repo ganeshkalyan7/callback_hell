7c918fff6cca1ada657e7d8e17264f9904e38c29
ganeshkalyan7 <92662581+ganeshkalyan7@users.noreply.github.com>
2022-01-03 15:13:06 +0530

    callback_hell

---
+++ script.js
@@ -0,0 +1,137 @@
+let v=10
+let button=document.getElementById('btn')
+button.addEventListener('click',()=>{
+    let output=document.getElementById('heading')
+    
+    setTimeout(()=>{
+        output.innerHTML=v;
+        output.style.backgroundColor='red'
+        output.style.color="white"
+        v--;
+        setTimeout(()=>{
+            output.innerHTML=v;
+            output.style.backgroundColor='Tomato'
+            output.style.color="black"
+            v--;
+            setTimeout(()=>{
+                output.innerHTML=v;
+                output.style.backgroundColor='Orange'
+                v--;
+                setTimeout(()=>{
+                    output.innerHTML=v;
+                    output.style.backgroundColor='DodgerBlue'
+                    output.style.color="black"
+                    v--;
+                    setTimeout(()=>{
+                        output.innerHTML=v;
+                        output.style.backgroundColor='MediumSeaGreen'
+                        output.style.color="black"
+                        v--;
+                        setTimeout(()=>{
+                            output.innerHTML=v;
+                            output.style.backgroundColor='MediumSeaGreen'
+                            output.style.color="black"
+                            v--;
+                            setTimeout(()=>{
+                                output.innerHTML=v;
+                                output.style.backgroundColor='Gray'
+                                output.style.color="black"
+                                v--;
+                                setTimeout(()=>{
+                                    output.innerHTML=v;
+                                    output.style.backgroundColor='SlateBlue'
+                                    output.style.color="black"
+                                    v--;
+                                    setTimeout(()=>{
+                                        output.innerHTML=v;
+                                        output.style.backgroundColor='Violet'
+                                    output.style.color="black"
+                                        v--;
+                                        setTimeout(()=>{
+                                            output.innerHTML=v;
+                                            output.style.backgroundColor='LightGray'
+                                            output.style.color="black"
+                                            v--;
+                                            setTimeout(()=>{
+                                                output.innerHTML=" Happy Independence Day ";
+                                                output.style.backgroundColor="black"
+                                                output.style.color="white"
+                        
+                                              
+
+                                            },1000)
+                            
+                                        },1000)
+                        
+                                    },1000)
+                    
+                                },1000)
+                
+                            },1000)
+            
+                        },1000)
+        
+                    },1000)
+    
+                },1000)
+
+            },1000)
+        },1000)
+
+    },500)
+
+
+
+
+})
+ 
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
